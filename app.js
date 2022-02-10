653f7d882d4fbc12720a7f8751ab0553f51d3b8d
LaceyBaughman <laceyebaughman@gmail.com>
2022-02-10 16:13:22 -0700

    level up

--- app.js
+++ app.js
@@ -3,58 +3,64 @@ const hero = {
 }
 
 const boss = {
-  health: 100
+  health: 100,
+  level: 1
 }
 
-function attackMonster(attack) {
-  let health = boss.health
-  if (health > 0) {
-    health -= 5
-  }
+function attackMonster() {
+  console.log('here is our attack function');
   if (boss.health > 0) {
+    boss.health -= 5
+  }
+  if (boss.health < 0) {
     boss.health = 0
   }
+  console.log('here is our boss health: ', boss.health);
+  //clamp
 
-  updateBossHealth(health)
+  updateBossHealth()
 }
 
-function updateBossHealth(health) {
-  let BossHealthElm = document.getElementById('Bosshealth')
-  //console.log(animalElm)
-  let bar = BossHealthElm.querySelector('progress-bar')
-  // console.log(bar);
+function updateBossHealth() {
+  let bar = document.querySelector('.progress-bar')
 
-  bar.style.width = boss[health].health + '%'
-  if (boss[health].health <= 0) {
-  }
+  bar.style.width = boss.health
+
+  drawBoss()
+  drawHero()
 }
 
 function drawHero() {
   let template = ''
-  for (let key in hero) {
-    let heroHealth = hero[key]
-    template += `
+  template += `
     <div class="progress">
-            <div class="progress-bar bg-danger" role="progressbar" style="width: 100%;" aria-valuenow="25">Hero</div>
+            <div class="progress-bar bg-danger" role="progressbar" style="width: ${hero.health}%" aria-valuenow="25">Hero</div>
           </div>
     `
-  }
+
   document.getElementById('HeroHealth').innerHTML = template
 }
 
+
 function drawBoss() {
   let template = ''
-  for (let key in boss) {
-    let bossHealth = boss[key]
-    template += ` 
+  template += ` 
     <div class="progress">
-    <div class="progress-bar bg-danger" role="progressbar" style="width: 100%;" aria-valuenow="25">Monster</div>
+    <div class="progress-bar bg-danger" role="progressbar" style="width: ${boss.health}%" aria-valuenow="25">Monster</div>
     </div>
     `
-  }
+
   document.getElementById('BossHealth').innerHTML = template
 }
 
+function reset() {
+  boss.level++
+  boss.health = 100 * boss.level
+  hero.health = 100
+  console.log('here is the boss level: ', boss.level);
+  console.log('here is the boss health: ', boss.health);
+
+}
 
 
 drawHero()
